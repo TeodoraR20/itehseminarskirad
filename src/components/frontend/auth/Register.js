36f7d5477f6a67b88a6857defada3c600d4e726f
Teodora Radovic <tradovic19@gmail.com>
2022-05-05 21:41:13 +0200

    Izmenjena registration stranica

--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from "axios";
+import swal from 'sweetalert';
+
 
 import Navbar from '../../../layouts/frontend/Navbar';
 
 function Register() {
+
+  const history = useHistory();
+
+  const[registerInput, setRegister] = useState({
+name: '',
+email:'',
+password: '',
+error_list: [],
+
+});
+
+
+const handleInput = (e) =>{
+
+e.presist();
+setRegister({...registerInput, [e.target.name]: e.target.value});
+
+}
+
+const registerSubmit = (e) => {
+
+e.preventDefault();
+
+const data = {
+
+name: registerInput.name,
+email: registerInput.email,
+password: registerInput.password,
+
+}
+
+axios.get('/sanctum/csrf-cookie').then(response => {
+  // Login...
+
+
+axios.post('/api/register',data).then(res =>{
+
+if(res.data.status === 200)
+
+{
+
+localStorage.setItem('auth_token',res.data.token);
+localStorage.setItem('auth_name',res.data.username);
+swal("Succes",res.data.message,"success");
+history.push('/');
+
+} else
+{
+   setRegister({...registerInput  ,error_list:res.data.validation_errors});
+}
+
+});
+});
+
+}
+
   return (
     <div>
 
@@ -25,35 +84,30 @@ function Register() {
 
 <div className="card-body">
 
-<form>
+<form onSubmit={registerSubmit}>
 
 <div className="form-group mb-3">
 
 <label>Full name</label>
-<input name="name" className="form-control"/>
-
+<input name="name" onChange={handleInput} value={registerInput.name} className="form-control"/>
+<span>{registerInput.error_list.name}</span>
 </div>
 
 <div className="form-group mb-3">
 
 <label>Email ID</label>
-<input name="email" className="form-control"/>
-
+<input name="email" onChange={handleInput} value={registerInput.email} className="form-control"/>
+<span>{registerInput.error_list.email}</span>
 </div>
 
 <div className="form-group mb-3">
 
 <label>Password</label>
-<input name="password" className="form-control"/>
-
+<input name="password" onChange={handleInput} value={registerInput.password} className="form-control"/>
+<span>{registerInput.error_list.password}</span>
 </div>
 
-<div className="form-group mb-3">
 
-<label>Confirm password</label>
-<input name="confirm_password" className="form-control"/>
-
-</div>
 
 
 <div className="form-group mb-3">
