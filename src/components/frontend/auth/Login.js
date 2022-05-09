67059dae2e817d5719f5bf452c3a25f36a41bb43
Teodora Radovic <tradovic19@gmail.com>
2022-05-09 21:41:53 +0200

    Dodata login stranica

--- src/components/frontend/auth/Login.js
+++ src/components/frontend/auth/Login.js
@@ -1,14 +1,82 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import Navbar from '../../../layouts/frontend/Navbar';
-function Login() {
+
+import axios from "axios";
+import swal from 'sweetalert';
+import { Route, Redirect, withRouter} from 'react-router-dom';
+
+function Login (props){
+
+  const {history} = props;
+
+const[loginInput,setLogin] = useState({
+
+email: '',
+password:'',
+error_list: [],
+
+});
+
+
+const handleInput = (e)  => {
+
+e.persist();
+setLogin({...loginInput,[e.target.name]: e.target.value});
+
+}
+const loginSubmit = (e) =>{
+
+e.preventDefault();
+
+const data = {
+
+email: loginInput.email,
+
+password: loginInput.password,
+}
+
+axios.get('/sanctum/csrf-cookie').then(response =>{
+axios.post('api/login',data).then(res=>{
+
+if(res.data.status ===200)
+{
+
+  localStorage.setItem('auth_token',res.data.token);
+  localStorage.setItem('auth_name',res.data.username);
+  swal("Success",res.data.message,"success");
+ history.push('/');
+
+}
+  
+else if(res.data.status === 401)
+{
+
+  swal("Warning",res.data.message,"warning");
+
+
+}
+
+else{
+
+  setLogin({...loginInput,error_list: res.data.validation_errors});
+
+}
+
+});
+
+});
+
+}
+ 
+
   return (
     <div>
 
 <Navbar />
 
 
-<div  className="cotainer py-5">
+<div  className="container py-5">
 
 <div className="row justify-content-center">
 
@@ -25,22 +93,22 @@ function Login() {
 
 <div className="card-body">
 
-<form>
+<form onSubmit={loginSubmit}>
 
 
 
 <div className="form-group mb-3">
 
 <label>Email ID</label>
-<input name="email" className="form-control"/>
-
+<input type="email" name="email" onChange={handleInput} value={loginInput.email} className="form-control"/>
+<span>{loginInput.error_list.email}</span>
 </div>
 
 <div className="form-group mb-3">
 
 <label>Password</label>
-<input name="password" className="form-control"/>
-
+<input  type = "password" name="password" onChange={handleInput} value={loginInput.password}  className="form-control"/>
+<span>{loginInput.error_list.password}</span>
 </div>
 
 
@@ -71,4 +139,4 @@ function Login() {
   )
 }
 
-export default Login
+export default withRouter(Login)
