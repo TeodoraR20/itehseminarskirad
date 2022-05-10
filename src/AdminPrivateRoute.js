f80d40af38bc2126eabc102a993fd33a9070e09a
Teodora Radovic <tradovic19@gmail.com>
2022-05-10 16:42:34 +0200

    Dodat loading...

--- src/AdminPrivateRoute.js
+++ src/AdminPrivateRoute.js
@@ -1,23 +1,61 @@
 import React from 'react'
 import {Route, Redirect} from 'react-router-dom'
 import MasterLayout from './layouts/admin/MasterLayout';
+import {useEffect, useState} from 'react'
+import axios from 'axios';
 
 function AdminPrivateRoute({...rest}) {
-  return (
-   
+const [Authenticated, setAuthenticated] = useState(false);
+const [loading, setLoading] = useState(true);
 
-<Route {...rest} 
-render = { ({props,location}) =>
-localStorage.getItem('auth_token') ? 
-( <MasterLayout {...props}/> ) :
-(<Redirect to={ {pathname: "/login", state: {from:location} } }/>)
+useEffect(() => {
+    axios.get('/api/checkingAuthenticated').then(res=>{
 
- }
- /> 
-  );
+if(res.status === 200)
+{
+    setAuthenticated(true);
+}
+
+setLoading(false);
+    });
+    
+
+  return () => {
+    setAuthenticated(false);
+  }
+}, []);
+
+if(loading)
+{
+    return <h1>Loading...</h1>
+}
+
+
+ return (
+
+<Route  {...rest}
+
+render = { ({props, location}) =>
+
+Authenticated ?
+(<MasterLayout {...props}/>) :
+(<Redirect to={{pathname: "/login", state:{from: location}}} />)
+
+
+} 
+
+
+
+
+/>
+
+ );
+
 
 }
 
-export default AdminPrivateRoute
+
+
+export default AdminPrivateRoute;
 
 
