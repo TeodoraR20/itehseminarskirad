8dc59a142a2c0ce25a37ea5e0f254f02c3666fbf
Teodora Radovic <tradovic19@gmail.com>
2022-05-10 14:09:19 +0200

    Dodat logout, redirect to

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from './components/frontend/Home';
 import Login from './components/frontend/auth/Login';
 
 import Register from './components/frontend/auth/Register';
+import {Redirect} from 'react-router-dom';
 
 import axios from 'axios';
 
@@ -17,6 +18,13 @@ axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function(config){
+
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+
+});
 
 
 function App() {
@@ -34,9 +42,19 @@ function App() {
 
 <Route exact  path="/" component={Home}/>
 
-<Route   path="/login" component={Login }/>
+{/*<Route   path="/login" component={Login}/>*/}
 
-<Route  path="/register" component={Register }/>
+{/*<Route  path="/register" component={Register }/>*/}
+
+<Route path="/login">
+
+  {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login />}
+</Route>
+
+<Route path="/register">
+
+  {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register />}
+</Route>
 
 <Route path="/admin" name="Admin" render={(props)  => <MasterLayout {...props}/>}  />
 {/*
--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -16,8 +16,8 @@ axios.post('/api/logout').then(res=>{
 
   if(res.data.status ===200)
   {
-    localStorage.removeItem('auth_token',res.data.token);
-    localStorage.removeItem('auth_name',res.data.username);
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('auth_name');
 
     swal("Success",res.data.message,"success");
     history.push('/');
