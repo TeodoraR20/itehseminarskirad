3a74f48ada1d7ab9a06cd0b4b7b102f6158b2b1a
Teodora Radovic <tradovic19@gmail.com>
2022-05-10 20:59:48 +0200

    Izmena za admin role

--- routes/api.php
+++ routes/api.php
@@ -20,14 +20,23 @@ Route::post('register',[AuthController::class, 'register']);
 
 Route::post('login',[AuthController::class, 'login']);
 
-Route::middleware(['auth:sanctum'])->group(function(){
+Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function(){
 
     Route::get('/checkingAuthenticated',function(){
 
 return response()->json(['message'=>'You are in.','status'=>200],200);
 
-    });
 
+
+    });
+ 
+});
+
+
+
+    Route::middleware(['auth:sanctum'])->group(function(){
+
+      
 
 Route::post('logout',[AuthController::class,'logout']);
 
@@ -40,5 +49,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //public routes
 
-Route::get('/student',[StudentController::class,'index']);
+
 
